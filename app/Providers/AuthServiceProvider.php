af7c95a8d3373df4f556d1638b46a35da8265001
Wadakai <wadakai1111@gmail.com>
2023-01-21 10:49:53 +0900

    認証設定を追加

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -24,7 +24,12 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        // メニュー編集権限
+        Gate::define('menu_editor', function ($user) {
+            if (($user->role & config('role.policy.menu_editor')) === 0) {
+                return false;
+            }
+            return true;
+        });
     }
 }
